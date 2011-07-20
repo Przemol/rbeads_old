2a23268ea3562af74c646c66313a4208eaf9f821
przemol <pstempor@gmail.com>
2011-07-20 12:02:59 +0100

    mappability <- various genome added

--- rBEADS_v2.0/source/MappabilityCorrection_v1.1.R
+++ rBEADS_v2.0/source/MappabilityCorrection_v1.1.R
@@ -4,23 +4,24 @@
 ###############################################################################
 
 
-MappabilityCorrection <- function(GCnormTrack, mappabilityTrack=NULL) {
+MappabilityCorrection <- function(GCnormTrack, mappabilityTrack=NULL, genome=Celegans) {
 	
-	if (is.null(mappabilityTrack)) {
-		catTime("Loading default mappability track", e={			
-			varname <- load("/Users/przemol/Documents/workspace/RBeads/rBeads/precalculated/MappabilityCe6_v2.Rdata")
-			assign("mappabilityTrack", get(varname))
-		})
-		
-	}
 	
 	catTime("Mappability correction", e={			
 		# 0) Old way (multyply than divide by the same value in DIV step)
-	#cov_GCmap <- cov * cmap.fd
+		#cov_GCmap <- cov * cmap.fd
 		# 1) masking regions of mappability lower than 100 (mappability score=0.25) from precalculated mappability track
 		#GCnormTrack[cmap.fd < 0.25] <- NA
-	names(GCnormTrack) <- seqnames(Celegans)[c(1:4,7,5,6)]
-	GCnormTrack[mappabilityTrack[c(1:4,7,5,6)] < 100] <- NA
+		if (genome@provider_version == 'ce6') {	
+			names(GCnormTrack) <- seqnames(Celegans)[c(1:4,7,5,6)]
+			GCnormTrack[mappabilityTrack[c(1:4,7,5,6)] < 100] <- NA
+		} else if (genome@provider_version == 'hg19') {	
+			GCnormTrack[mappabilityTrack < 100] <- NA
+		} else if (genome@provider_version == 'mm9') {
+			GCnormTrack[mappabilityTrack < 100] <- NA
+		} else {
+			warning('Unnkonown genome!')
+		}
 		# 2) masking regions within lowest 5% of all read values
 	# cov_GCmap[cov.r <= quantile(as.vector(cov.r), 0.05)] <- NA			
 	})
